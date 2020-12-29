cd0f150dcf9d37da91299d58e11f3fdaa88facd5
rwitam <rwitam.cloud@gmail.com>
2020-12-29 14:54:31 +0530

    Commit For Spring Security Demo 09

---
+++ spring-security-demo-09-jdbc-bcrypt/src/main/java/org/springsecurity/demo/config/SecurityWebApplicationInitializer.java
@@ -0,0 +1,8 @@
+package org.springsecurity.demo.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer 
+                              extends AbstractSecurityWebApplicationInitializer {
+
+}
